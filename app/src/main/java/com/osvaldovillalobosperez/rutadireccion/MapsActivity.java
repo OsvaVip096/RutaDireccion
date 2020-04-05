1bb554ddf16464d492eeaa7d0e9d343d19ba01bb
OsvaX <s15120062@alumnos.itsur.edu.mx>
2020-04-05 16:12:14 -0500

    Se agrego el requerimiento de permisos en tiempo de ejecución y el permiso de Internet en el Manifest.

--- app/src/main/java/com/osvaldovillalobosperez/rutadireccion/MapsActivity.java
+++ app/src/main/java/com/osvaldovillalobosperez/rutadireccion/MapsActivity.java
@@ -1,30 +1,253 @@
 package com.osvaldovillalobosperez.rutadireccion;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
-import android.os.Bundle;
-
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
+    private static final int PERMISSIONS_REQUEST_CODE = 1001;
+    private static final int MY_PERMISSION_REQUEST_API = 1002;
+
+    String[] appPermissions = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        createLocationRequest();
+
+        // TODO: Verifica permisos en tiempo de ejecución.
+        if (checkAndRequestPermissions()) {
+            // El permiso ya ha sido concedido.
+
+            // Obtain the SupportMapFragment and get notified when the
+            // map is ready to be used.
+            SupportMapFragment mapFragment =
+                    (SupportMapFragment) getSupportFragmentManager()
+                            .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
     }
 
+    public boolean checkAndRequestPermissions() {
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String perm : appPermissions) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    perm
+            ) != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(perm);
+            }
+        }
+
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    listPermissionsNeeded.toArray(
+                            new String[listPermissionsNeeded.size()]
+                    ), PERMISSIONS_REQUEST_CODE
+            );
+            return false;
+        }
+
+        return true;
+    }
+
+    public AlertDialog showDialog(String title, String msg, String positiveLavel,
+                                  DialogInterface.OnClickListener positiveOnClick,
+                                  String negativeLavel,
+                                  DialogInterface.OnClickListener negativeOnClick,
+                                  boolean isCancelAble) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title);
+        builder.setCancelable(isCancelAble);
+        builder.setMessage(msg);
+        builder.setPositiveButton(positiveLavel, positiveOnClick);
+        builder.setNegativeButton(negativeLavel, negativeOnClick);
+
+        AlertDialog alert = builder.create();
+        alert.show();
+        return alert;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            String[] permissions,
+            int[] grantResults
+    ) {
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            HashMap<String, Integer> permissionResult = new HashMap<>();
+            int deniedCount = 0;
+
+            for (int i = 0; i < grantResults.length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                    permissionResult.put(permissions[i], grantResults[i]);
+                    deniedCount++;
+                }
+            }
+
+            if (deniedCount == 0) {
+                // Todos los permisos (contador) son correctos.
+            } else {
+                for (Map.Entry<String, Integer> entry : permissionResult.entrySet()) {
+                    String permName = entry.getKey();
+                    int permResult = entry.getValue();
+
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(
+                            this,
+                            permName)
+                    ) {
+                        showDialog(
+                                "",
+                                "Esta aplicación necesita permisos ubicación del Teléfono",
+                                "Si, otorgar permisos",
+                                new DialogInterface.OnClickListener() {
+
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                        checkAndRequestPermissions();
+                                    }
+                                },
+                                "No otorgar permisos",
+                                new DialogInterface.OnClickListener() {
+
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                        finish();
+                                    }
+                                },
+                                false
+                        );
+                    } else {
+                        showDialog(
+                                "",
+                                "Has negado algun(os) permiso(s), concede todos los " +
+                                        "permisos en Ajustes",
+                                "Ir a Ajustes",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Intent intent = new Intent(
+                                                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                                                Uri.fromParts(
+                                                        "package",
+                                                        getPackageName(),
+                                                        null)
+                                        );
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                },
+                                "No, salir",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                        finish();
+                                    }
+                                },
+                                false
+                        );
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    protected void createLocationRequest() {
+        /* Primera parte. */
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        /* Segunda parte. */
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // Esta bien.
+                /*Toast.makeText(
+                        MainActivity.this,
+                        "El SuccessListener de LocationSettingsResponse funciono" +
+                                " correctamente",
+                        Toast.LENGTH_LONG
+                ).show();*/
+            }
+        });
+
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    // Las instancias no están satisfechas.
+                    try {
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(
+                                MapsActivity.this,
+                                MY_PERMISSION_REQUEST_API
+                        );
+                        /*Toast.makeText(
+                                MainActivity.this,
+                                "Fallo el Listener, se necesita la activación de la API",
+                                Toast.LENGTH_LONG
+                        ).show();*/
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignorar el error.
+                    }
+                }
+            }
+        });
+    }
 
     /**
      * Manipulates the map once available.
